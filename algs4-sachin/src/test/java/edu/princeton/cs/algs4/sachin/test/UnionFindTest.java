533f9e4e0fe04c2da1a61ff2f350f7aa4565a970
Sachin Bhosale <2748549+sachinb4u@users.noreply.github.com>
2018-10-07 22:24:11 +0530

    Sample Unit test added

--- algs4-sachin/src/test/java/edu/princeton/cs/algs4/sachin/test/UnionFindTest.java
+++ algs4-sachin/src/test/java/edu/princeton/cs/algs4/sachin/test/UnionFindTest.java
@@ -1,5 +1,21 @@
 package edu.princeton.cs.algs4.sachin.test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Test;
+
 public class UnionFindTest {
 
+	@Test
+	public void run_quickfind_tiny() throws FileNotFoundException {
+		File tinyFile = new File("src/main/resources/tinyUF.txt");
+		Scanner scanner = new Scanner(tinyFile);
+		
+		int N = scanner.nextInt();
+		System.out.println("Total Records : " + N);
+		
+		scanner.close();
+	}
 }
