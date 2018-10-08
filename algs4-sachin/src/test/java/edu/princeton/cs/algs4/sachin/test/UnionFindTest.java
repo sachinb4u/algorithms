6766c52f464eea1218b4400d479412cd65ddb013
Sachin Bhosale <s.bhosale@sap.com>
2018-10-08 14:59:35 +0530

    Clients written for UF using Junit testcases

--- algs4-sachin/src/test/java/edu/princeton/cs/algs4/sachin/test/UnionFindTest.java
+++ algs4-sachin/src/test/java/edu/princeton/cs/algs4/sachin/test/UnionFindTest.java
@@ -11,12 +11,17 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.sachin.CommonUtil;
 import edu.princeton.cs.algs4.sachin.QuickFind;
 import edu.princeton.cs.algs4.sachin.QuickUnion;
+import edu.princeton.cs.algs4.sachin.WeightedQuickUnion;
 
 public class UnionFindTest {
 
+	private static final String TINY_UF = "src/main/resources/tinyUF.txt";
+//	private static final String MEDIUM_UF = "src/main/resources/mediumUF.txt";
+//	private static final String LARGE_UF = "src/main/resources/largeUF.txt";
+
 	@Test
 	public void run_quickfind_tiny() throws IOException {
-		Scanner scanner = new Scanner(Paths.get("src/main/resources/tinyUF.txt"), "utf-8");
+		Scanner scanner = new Scanner(Paths.get(TINY_UF));
 		StdIn.setScanner(scanner);
 		
 		int N = StdIn.readInt();
@@ -39,7 +44,7 @@ public class UnionFindTest {
 	
 	@Test
 	public void run_quickunion_tiny() throws IOException {
-		Scanner scanner = new Scanner(Paths.get("src/main/resources/tinyUF.txt"), "utf-8");
+		Scanner scanner = new Scanner(Paths.get(TINY_UF));
 		StdIn.setScanner(scanner);
 		
 		int N = StdIn.readInt();
@@ -58,5 +63,28 @@ public class UnionFindTest {
 			StdOut.println(CommonUtil.showUFArray(quickUnion.getId()));
 		}
 		StdOut.println(quickUnion.count() + " components");
+	}
+	
+	@Test
+	public void run_weighted_quick_union_tiny() throws IOException {
+		Scanner scanner = new Scanner(Paths.get(TINY_UF));
+		StdIn.setScanner(scanner);
+		
+		int N = StdIn.readInt();
+
+		WeightedQuickUnion weightedQuickUnion = new WeightedQuickUnion(N);
+
+		for (int i = 0; i < N; i++) {
+			int p = StdIn.readInt();
+			int q = StdIn.readInt();
+
+			if (weightedQuickUnion.connected(p, q))
+				continue;
+
+			weightedQuickUnion.union(p, q);
+			StdOut.println(p + " " + q);
+			StdOut.println(CommonUtil.showUFArray(weightedQuickUnion.getId()));
+		}
+		StdOut.println(weightedQuickUnion.count() + " components");
 	}
 }
